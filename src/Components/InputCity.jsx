1c45d74960d3c3e47c121d554b7311f5e33ea9e3
leighembee <leigh.blechman@gmail.com>
2024-03-04 12:25:57 -0500

    add reqs for inputcity comp

---
+++ src/Components/InputCity.jsx
@@ -0,0 +1,9 @@
+/*
+- Provides user with a search box where they can search weather
+  information by city name
+- Component contains input field and a submit button
+- Input takes two attributes:
+  - city name (value)
+  - handler function - handles the modification of the city name for onChange attribute
+- Submit button to initiate API call
+*/
