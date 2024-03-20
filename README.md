00f7e03629a97df962eef11f1e38c1660b5774cd
leighembee <leigh.blechman@gmail.com>
2024-03-20 11:49:27 -0400

    add handlers for input component

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - [ ] update app with preferred react practices
 - [ ] create proper utils files
 - [ ] convert to typescript
+- [ ] add option for identifying current location
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import './styles.css';
 import Header from './Components/Header';
 import InputCity from './Components/InputCity';
 
-const App = () => (
-  <div>
-    <Header />
-    <InputCity />
-  </div>
-);
+// add state for city name
+// handle input and handle submit
+
+const App = () => {
+  const [inputCity, setInputCity] = useState('New York');
+  const [cityName, setCityName] = useState('New York');
+  const inputHandler = (e) => setInputCity(e.target.value);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setCityName(inputCity);
+  };
+
+  return (
+    <div>
+      <Header />
+      <InputCity
+        city={inputCity}
+        onInputHandler={inputHandler}
+        onSubmitHandler={submitHandler}
+      />
+    </div>
+  );
+};
 
 export default App;
